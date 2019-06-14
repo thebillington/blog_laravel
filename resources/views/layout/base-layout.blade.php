ccdd11c6087f88dc4bd3b78aea764a89f33f4a8c
Billy Rebecchi <billy@appoly.co.uk>
2019-06-14 14:18:01 +0100

    Switch to bootstrap and finish basic layout

--- resources/views/layout/base-layout.blade.php
+++ resources/views/layout/base-layout.blade.php
@@ -5,6 +5,8 @@
     <head>
 
         <title>@yield('page-title', 'Appoly Blog')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <script src="{{ asset('js/app.js') }}"></script>
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
     </head>
@@ -17,27 +19,47 @@
     			<img src="{{ asset('res/header-img.jpg') }}">
     		</div>
 
-    		<div id="navBar">
-    			<img class="logo" src="{{ asset('res/logo.png.webp') }}">
-    			<ul>
-    				<li>About</li>
-    				<li>Posts</li>
-    			</ul>
-    		</div>
+
+    		<div id="navContainer" class="orange-bg">
+    			<div class='container'>
+		    		<nav class="navbar navbar-expand-lg navbar-light">
+		    			<a class="navbar-brand" href="https://appoly.co.uk"><img class="logo" src="{{ asset('res/logo.png.webp') }}"></a>
+		    			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+		    			<div class="collapse navbar-collapse" id="navbar">
+			    			<ul class="navbar-nav mr-auto">
+			    				<li class="nav-item">About</li>
+			    				<li class="nav-item">Posts</li>
+			    			</ul>
+		    			</div>
+		    		</nav>
+	    		</div>
+	    	</div>
 
     	</div>
 
     	<div id="contentWrapper">
 
-    		<div id="contentScroll">
+    		<div class="container">
 
-    			<div id="content">
-    
-	        		@yield('page-content')
+	    		<div id="contentScroll">
 
-	        	</div>
+	    			<div id="content">
+	    
+		        		@yield('page-content')
 
-	    	</div>
+		        	</div>
+
+		        	<div id="sidebar">
+
+
+
+		        	</div>
+
+		    	</div>
+
+		    </div>
 
     	</div>
 
